d525ce921b9b58028756244f9bb285dfc830c75f
Vivek80025 <vjha052003@gmail.com>
2025-06-30 12:08:42 +0530

    finished1

---
+++ quizzes-notes_app_frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return <div className="container col-xxl-8 px-4 py-5" >
+  <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div className="col-10 col-sm-8 col-lg-6">
+      <img src="image/quizphoto.jpg" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+    </div>
+    <div className="col-lg-6">
+      <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Boost Your Learning Journey with Interactive Quizzes & Study Notes</h1>
+      <p className="lead">Engage in interactive quizzes across various subjects and easily access detailed notes to enhance your learning experience. Our platform offers a user-friendly interface with a wide range of quizzes, covering numerous topics, and comprehensive notes that help you understand key concepts effectively.</p>
+    </div>
+  </div>
+</div>
+}
+
+export default Home
